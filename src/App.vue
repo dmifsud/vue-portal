2faf2420cdd8397a4ec86ad05f621ab1c5fd9d41
David Mifsud <david.mifsud89@gmail.com>
2023-03-25 11:06:30 +0100

    Add new component users

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+  <div class="main-container">
+      <UsersComponent/>
+  </div>
+
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
+import UsersComponent from './components/Users.vue';
+
 
 @Options({
   components: {
     HelloWorld,
+    UsersComponent
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+
+    
+
+}
 </script>
 
 <style>
@@ -20,8 +31,14 @@ export default class App extends Vue {}
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.main-container {
+    min-width: 900px;
+    width: 80%;
+    margin: 20vh auto;
 }
 </style>
---
+++ src/components/Users.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-card title="Users" :loading="loading">
+        <v-table height="300px">
+            <thead>
+            <tr>
+                <th class="text-left">
+                Name
+                </th>
+                <th class="text-left">
+                Calories
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr
+                v-for="item in desserts"
+                :key="item.name"
+            >
+                <td>{{ item.name }}</td>
+                <td>{{ item.calories }}</td>
+            </tr>
+            </tbody>
+        </v-table>
+      </v-card>
+</template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+  @Options({})
+  export default class UsersComponent extends Vue {
+
+    public loading = true;
+
+    public desserts: { name: string; calories: number}[] = []
+
+    public mounted(): void {
+        setTimeout(() => {
+            this.loading = false;
+            this.desserts = [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },
+        ];
+        }, 1000);
+    }
+    
+  }
+
+</script>
+
+<style>
+  .users {
+
+  }
+</style>
